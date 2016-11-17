6c303b338b84f081b5bb21e158c076934a5082fc
Tomoaki NISHIYAMA <tomoakin@kenroku.kanazawa-u.ac.jp>
2016-11-17 16:41:16 +0900

    array manager

---
+++ fp_array.c
@@ -0,0 +1,37 @@
+#include <malloc.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "fp_array.h"
+
+fp_array* 
+fp_array_init(fp_array*fp_a)
+{
+  size_t default_size=100;
+  fp_a->used_slots = 0;
+  fp_a->base = malloc(sizeof(FILE*)*default_size);
+  fp_a->allocated_slots = default_size;
+}
+
+fp_array*
+fp_array_expand(fp_array*fp_a, size_t new_size)
+{
+  fp_a->base = realloc(fp_a->base, sizeof(FILE*)*new_size);
+  fp_a->allocated_slots = new_size;
+}
+
+
+int fp_array_set(fp_array* fp_a, int i, FILE* fp)
+{
+  if(i <= 0) return i;
+  if(fp_a->allocated_slots <= i)fp_array_expand(fp_a,i+100);
+  fp_a->base[i]=fp;
+  return i;
+}
+
+FILE* fp_array_get(fp_array* fp_a, int i)
+{
+  if(i <= 0 || i >= fp_a -> allocated_slots) return NULL;
+  return fp_a->base[i];
+};
+
---
+++ fp_array.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "trie.h"
+
+/* public declarations */
+
+typedef struct _fp_array{
+  FILE** base;
+  size_t used_slots;
+  size_t allocated_slots;
+} fp_array;
+
+fp_array* name_array_init(fp_array*);
+
+int fp_array_set(fp_array*, int, FILE*);
+
+FILE* fp_array_get(fp_array*, int);
